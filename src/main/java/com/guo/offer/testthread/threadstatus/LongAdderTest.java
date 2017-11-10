399685ff6af01ceb2ecb1480597fec134631e249
guozifeng <602394902@qq.com>
2017-11-11 00:09:49 +0800

    test

---
+++ src/main/java/com/guo/offer/testthread/threadstatus/LongAdderTest.java
@@ -0,0 +1,21 @@
+package com.guo.offer.testthread.threadstatus;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
+
+public class LongAdderTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LongAdder a = new LongAdder();
+		a.add(1);
+		
+		AtomicLong count = new AtomicLong();
+		count.addAndGet(1);
+		
+		System.out.println(a);
+		System.out.println(count);
+	}
+
+}
