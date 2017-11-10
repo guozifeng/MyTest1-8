393b0ec0b4aa8ffa9cd4475366a7a87b14a071f3
guozifeng <602394902@qq.com>
2017-11-10 19:07:03 +0800

    java8

--- src/main/java/com/guo/MyTest18/Java8StreamTest.java
+++ src/main/java/com/guo/MyTest18/Java8StreamTest.java
@@ -19,19 +19,25 @@ import com.google.common.collect.Lists;
 public class Java8StreamTest {
 
 	public static void main(String[] args) {
-		Test5();Test4();
-		List<Integer> ints = Lists.newArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+		Test5();
+		/*
+		 * Test4(); List<Integer> ints = Lists.newArrayList(1, 2, 3, 4, 5, 6, 7,
+		 * 8, 9, 10);
+		 * 
+		 * System.out.println("ints sum is:" + ints.stream().reduce((sum, item)
+		 * -> sum + item).get()); System.out.println("ints sum is:" +
+		 * ints.stream().reduce(0, (sum, item) -> sum + item));
+		 * 
+		 * List<Integer> nums = Lists.newArrayList(1, 1, null, 2, 3, 4, null, 5,
+		 * 6, 7, 8, 9, 10);
+		 * 
+		 * List<Integer> numsWithoutNull = nums.stream().filter(num -> num !=
+		 * null).collect(() -> new ArrayList<Integer>(), (list, item) ->
+		 * list.add(item), (list1, list2) -> list1.addAll(list2));
+		 * 
+		 * System.out.println(numsWithoutNull);
+		 */
 
-		System.out.println("ints sum is:" + ints.stream().reduce((sum, item) -> sum + item).get());
-		System.out.println("ints sum is:" + ints.stream().reduce(0, (sum, item) -> sum + item));
-		
-		List<Integer> nums = Lists.newArrayList(1, 1, null, 2, 3, 4, null, 5, 6, 7, 8, 9, 10);
-
-		List<Integer> numsWithoutNull = nums.stream().filter(num -> num != null).collect(() -> new ArrayList<Integer>(),
-				(list, item) -> list.add(item), (list1, list2) -> list1.addAll(list2));
-		
-		System.out.println(numsWithoutNull);
-		
 	}
 
 	private static void Test5() {
@@ -41,37 +47,43 @@ public class Java8StreamTest {
 				new Item("watermelon", 10, new BigDecimal("29.99")), new Item("papaya", 20, new BigDecimal("9.99")),
 				new Item("apple", 10, new BigDecimal("9.99")), new Item("banana", 10, new BigDecimal("19.99")),
 				new Item("apple", 20, new BigDecimal("9.99")));
-
+		List<Item> itemlist = new ArrayList();
 		// group by price
-		Map<String, Map<Integer, Item>> groupByPriceMap = items.stream().collect(Collectors.groupingBy(Item::getName,
-				Collectors.groupingBy(Item::getQty, Collectors.reducing(new Item(),(Item a )-> {
-					return a;
-					},(Item v1, Item v2) -> {
-					Item v3= new Item();
-					
-					if(v2.getPrice() != null){
-						v3.setPrice(v2.getPrice());
-					}
-					
-					System.out.println(v3);
-					return v3;
-				}))));
+		items.stream()
+				.collect(Collectors.groupingBy(Item::getName, Collectors.groupingBy(Item::getQty)))
+				.forEach((name, map) -> {
+					System.out.println(name + "------------");
+					map.forEach((a, b) -> {
+						// System.out.println(a+"++++++++++"+b);
+						Item aaa = b.stream().reduce(new Item(), (aa, bb) -> {
+							aa.setName(bb.getName());
+							if (aa.getPrice() != null) {
+								aa.setPrice(aa.getPrice().add(bb.getPrice()));
+							} else {
+								aa.setPrice(bb.getPrice());
+							}
+							aa.setQty(bb.getQty());
+							System.out.println(aa + "*************");
 
-		System.out.println(groupByPriceMap);
+							return aa;
+						});
+						itemlist.add(aaa);
+					});
+				});
 
+		System.out.println(itemlist);
 		// group by price, uses 'mapping' to convert List<item> to Set<string>
 		Map<BigDecimal, Set<String>> result = items.stream()
 				.collect(Collectors.groupingBy(Item::getPrice, Collectors.mapping(Item::getName, Collectors.toSet())));
 
 		System.out.println(result);
-		
+
 		Map<String, Map<Integer, List<Item>>> result1 = items.stream()
 				.collect(Collectors.groupingBy(Item::getName, Collectors.groupingBy(Item::getQty)));
 
 		System.out.println(result1);
-		
-	}
 
+	}
 
 	private static void Test4() {
 		// 3 apple, 2 banana, others 1
@@ -91,12 +103,11 @@ public class Java8StreamTest {
 
 		System.out.println(sum);
 
-		Map<String, Map<Integer, BigDecimal>> sum1 = items.stream()
-				.collect(Collectors.groupingBy(Item::getName, Collectors.groupingBy(Item::getQty,
-						Collectors.reducing(BigDecimal.ZERO, (Item a ) -> {
-							System.out.println(a);
-							return a.getPrice();
-						}, (x, y) -> x.add(y)))));
+		Map<String, Map<Integer, BigDecimal>> sum1 = items.stream().collect(Collectors.groupingBy(Item::getName,
+				Collectors.groupingBy(Item::getQty, Collectors.reducing(BigDecimal.ZERO, (Item a) -> {
+					System.out.println(a);
+					return a.getPrice();
+				}, (x, y) -> x.add(y)))));
 
 		System.out.println(sum1);
 	}
