8b6d566e903105c288c8fa406af28b13cf8053d8
guozifeng <602394902@qq.com>
2017-11-11 22:24:11 +0800

    test

--- src/main/java/com/guo/offer/testmath/TestMath.java
+++ src/main/java/com/guo/offer/testmath/TestMath.java
@@ -2,33 +2,13 @@ package com.guo.offer.testmath;
 
 /**
  * 
- * Math类中提供了三个与取整有关的方法：ceil,floor,round, 这些方法的作用于它们的英文名称的含义相对应，
- * 例如：ceil的英文意义是天花板，该方法就表示向上取整， Math.ceil（11.3）的结果为12，
- * Math.ceil(-11.6)的结果为-11；
- * floor的英文是地板，该方法就表示向下取整，Math.floor(11.6)的结果是11， 
- * Math.floor(-11.4)的结果-12；
- * 最难掌握的是round方法，他表示“四舍五入”， 算法为Math.floor(x+0.5),
- * 即将原来的数字加上0.5后再向下取整，
- * 所以，Math.round(11.5)的结果是12，Math.round(-11.5)的结果为-11. 
- * Math.round( * )符合这样的规律：小数点后大于5全部加，等于5正数加，小于5全不加。 
- * 让我们看看JDK的说明： (1)public static long
- * round(double a) returns the closest long to the argument. the result is
- * rounded to an integer by adding 1/2, taking the floor of the result, and
- * casting the result to type long. in other words, the result is equal to the
- * value of the expression:
+ * Math类中提供了三个与取整有关的方法：ceil,floor,round, 这些方法的作用于 它们的英文名称的含义相对应，
+ * 例如：ceil的英文意义是天花板，该方法就表示向上取整， Math.ceil（11.3）的结果为12， Math.ceil(-11.6)的结果为-11；
+ * floor的英文是地板，该方法就表示向下取整，Math.floor(11.6)的结果是11， Math.floor(-11.4)的结果-12；
+ * 最难掌握的是round方法，他表示“四舍五入”， 算法为Math.floor(x+0.5), 即将原来的数字加上0.5后再向下取整，
+ * 所以，Math.round(11.5)的结果是12，Math.round(-11.5)的结果为-11. Math.round( *
+ * )符合这样的规律：小数点后大于5全部加，等于5正数加，小于5全不加。
  * 
- * (long)math.floor(a + 0.5d)
- * 
- * (2)public static double floor(double a) returns the largest(closest to
- * positive infinity) double value that is not greater than the argument and is
- * equal to a mathematical integer.special cases: if the argument value is
- * already equal to a mathematical integer, then the result is the same as the
- * argument. if the argument is nan or an infinity or positive zero or negative
- * zero, then the result is the same as the argument.
- * 
- * parameters: a - a value. returns: the smallest (closest to negative infinity)
- * floating-point value that is not less than the argument and is equal to a
- * mathematical integer.
  * 
  * @author Administrator
  *
@@ -43,8 +23,9 @@ public class TestMath {
 		test1();
 		test2();
 	}
-	public static void test1(){
-		
+
+	public static void test1() {
+
 		int i = 0;
 		for (int j = 0; j < 10; j++) {
 			i = i++;
@@ -55,9 +36,10 @@ public class TestMath {
 		// 做自增
 		i = i++;
 		System.out.println("i的最后结果" + temp);
-		
+
 	}
-	public static void test2(){
+
+	public static void test2() {
 		System.out.println(Math.round(-11.52));
 
 		// Math.round():Java中的四舍五入函数
@@ -84,7 +66,8 @@ public class TestMath {
 
 		System.out.println("也就是说：小数点后大于5全部加，等于5正数加，小于5全不加");
 	}
-	public static void test20(){
+
+	public static void test20() {
 		System.out.println("90 度的正弦值：" + Math.sin(Math.PI / 2));
 		System.out.println("0度的余弦值：" + Math.cos(0));
 		System.out.println("60度的正切值：" + Math.tan(Math.PI / 3));
