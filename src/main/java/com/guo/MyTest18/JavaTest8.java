6c72e3da6e8f9a951af0cd39db6381538ffe479a
guozifeng <602394902@qq.com>
2017-11-12 00:03:34 +0800

    test

---
+++ src/main/java/com/guo/MyTest18/JavaTest8.java
@@ -0,0 +1,46 @@
+package com.guo.MyTest18;
+
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class JavaTest8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Function<String, String> function = (x) -> {
+			System.out.print(x + ": ");
+			return "Function";
+		};
+		System.out.println(function.apply("hello world1"));
+
+		// Predicate<T> -T作为输入，返回的boolean值作为输出
+		Predicate<String> pre = (x) -> {
+			System.out.print(x);
+			return false;
+		};
+		System.out.println(": " + pre.test("hello World2"));
+
+		// Consumer<T> - T作为输入，执行某种动作但没有返回值
+		Consumer<String> con = (x) -> {
+			System.out.println(x);
+		};
+		con.accept("hello world3");
+
+		// Supplier<T> - 没有任何输入，返回T
+		Supplier<String> supp = () -> {
+			return "Supplier";
+		};
+		System.out.println(supp.get());
+
+		// BinaryOperator<T> -两个T作为输入，返回一个T作为输出，对于“reduce”操作很有用
+		BinaryOperator<String> bina = (x, y) -> {
+			System.out.print(x + "====" + y);
+			return "BinaryOperator";
+		};
+		System.out.println("+++" + bina.apply("hello1 ", "world5"));
+	}
+
+}
