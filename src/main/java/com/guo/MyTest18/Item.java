1f56272b118e5b8d08c8f2e03353e75fc9c3c365
guozifeng <602394902@qq.com>
2017-11-12 10:45:51 +0800

    test

--- src/main/java/com/guo/MyTest18/Item.java
+++ src/main/java/com/guo/MyTest18/Item.java
@@ -13,6 +13,7 @@ public class Item {
 		this.name = "";
 		this.price = new BigDecimal("0");
 		this.qty = 0;
+		
 		//System.out.println("-------------");
 	}
 	public Item(String name, Integer qty, BigDecimal price) {
