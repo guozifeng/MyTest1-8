6888a315c6f45f95f6fff675fab26d59b7f4f266
guozifeng <602394902@qq.com>
2017-11-18 00:11:44 +0800

    test

--- src/main/java/com/guo/MyTest18/JavaTest8.java
+++ src/main/java/com/guo/MyTest18/JavaTest8.java
@@ -1,5 +1,6 @@
 package com.guo.MyTest18;
 
+import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -10,7 +11,7 @@ public class JavaTest8 {
 
 	public static void main(String[] args) {
 
-		//第一个参数是输入，第二个参数是输出。
+		// 第一个参数是输入，第二个参数是输出。
 		Function<String, Integer> function = (x) -> {
 			System.out.print(x + ": ");
 			return 2222;
@@ -27,8 +28,15 @@ public class JavaTest8 {
 		// Consumer<T> - T作为输入，执行某种动作但没有返回值
 		Consumer<String> con = (x) -> {
 			System.out.println(x);
-		}; 
+		};
 		con.accept("hello world3");
+
+		// BiConsumer<T,U> - T,U作为输入，执行某种动作但没有返回值
+		BiConsumer<String, Integer> bicon = (x, y) -> {
+			System.out.println(x);
+			System.out.println(y);
+		};
+		bicon.accept("guozifeng", 111);
 
 		// Supplier<T> - 没有任何输入，返回T
 		Supplier<String> supp = () -> {
