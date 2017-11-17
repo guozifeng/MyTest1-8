f5daa2ede3f12165cf232ce3a53b13bffdc3b46e
guozifeng <602394902@qq.com>
2017-11-18 00:01:07 +0800

    test

--- src/main/java/com/guo/MyTest18/Java8StreamTest.java
+++ src/main/java/com/guo/MyTest18/Java8StreamTest.java
@@ -19,9 +19,9 @@ import com.google.common.collect.Lists;
 public class Java8StreamTest {
 
 	public static void main(String[] args) {
-		Test5();
+		//Test5();
 
-		Test4();
+		//Test4();
 		List<Integer> ints = Lists.newArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
 		System.out.println("ints sum is:" + ints.stream().reduce((sum, item) -> sum + item).get());
