3a17159aff8eb6b6a47380f6556fd1986d9bb069
guozifeng <602394902@qq.com>
2017-11-19 22:43:13 +0800

    test

--- src/main/java/com/guo/offer/testeasyapi/TestExcel.java
+++ src/main/java/com/guo/offer/testeasyapi/TestExcel.java
@@ -1,48 +1,34 @@
 package com.guo.offer.testeasyapi;
 
-import java.io.FileInputStream;
-import java.sql.Connection;
-
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.poifs.filesystem.POIFSFileSystem;
-
-import com.alibaba.fastjson.JSONObject;
-import com.guo.MyTest18.User;
-import com.guo.offer.testSimpleDateFormat.DateUtil;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class TestExcel {
 	public static void main(String[] args) throws Exception {
 		test1();
 	}
-	public static void test1() throws Exception{
-		//此时的Workbook应该是从 客户端浏览器上传过来的 uploadFile了,其实跟读取本地磁盘的一个样  
-		String userUploadFile = "";
-        POIFSFileSystem fs=new POIFSFileSystem(new FileInputStream(userUploadFile));  
-        HSSFWorkbook wb=new HSSFWorkbook(fs);  
-        HSSFSheet hssfSheet=wb.getSheetAt(0);  
-          
-        if(hssfSheet!=null){  
-            //遍历excel,从第二行开始 即 rowNum=1,逐个获取单元格的内容,然后进行格式处理,最后插入数据库  
-            for(int rowNum=1;rowNum<=hssfSheet.getLastRowNum();rowNum++){  
-                HSSFRow hssfRow=hssfSheet.getRow(rowNum);  
-                if(hssfRow==null){  
-                    continue;  
-                }  
-                System.out.println();
-                System.out.println(ExcelUtil.formatCell(hssfRow.getCell(0)));  
-                System.out.println(ExcelUtil.formatCell(hssfRow.getCell(1)));  
-                System.out.println(ExcelUtil.formatCell(hssfRow.getCell(2)));  
-                System.out.println(ExcelUtil.formatCell(hssfRow.getCell(3)));  
 
-                //对于单元格日期需要进行特殊处理  
-                System.out.println(DateUtil.formatString(ExcelUtil.formatCell2(hssfRow.getCell(4)), "yyyy-MM-dd"));  
-
-            }  
-        }  
-        JSONObject result=new JSONObject();  
-        result.put("success", "true");  
+	public static void test1() throws Exception {
+		// 此时的Workbook应该是从 客户端浏览器上传过来的 uploadFile了,其实跟读取本地磁盘的一个样
+		String strPath = "C:\\Users\\Administrator\\Desktop\\a.xlsx";
+		// 构造 XSSFWorkbook 对象，strPath 传入文件路径
+		XSSFWorkbook xwb = new XSSFWorkbook(strPath);
+		// 读取第一章表格内容
+		XSSFSheet sheet = xwb.getSheetAt(0);
+		// 定义 row、cell
+		XSSFRow row;
+		String cell;
+		// 循环输出表格中的内容
+		for (int i = sheet.getFirstRowNum(); i < sheet.getPhysicalNumberOfRows(); i++) {
+			row = sheet.getRow(i);
+			for (int j = row.getFirstCellNum(); j < row.getPhysicalNumberOfCells(); j++) {
+				// 通过 row.getCell(j).toString() 获取单元格内容，
+				cell = row.getCell(j).toString();
+				System.out.print(cell + "\t");
+			}
+			System.out.println("");
+		}
 
 	}
 }
