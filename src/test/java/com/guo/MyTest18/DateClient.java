5c4d8205650b915667b3b5adbaecd4ea771c91e9
guozifeng <602394902@qq.com>
2017-11-20 23:46:20 +0800

    test

--- src/test/java/com/guo/MyTest18/DateClient.java
+++ src/test/java/com/guo/MyTest18/DateClient.java
@@ -18,12 +18,8 @@ public class DateClient {
 	
 	@Test
 	public void test1() {
-		DateTimeFormatter dtf = DateTimeFormatter.ISO_DATE_TIME;
+		
         LocalDateTime ldt = LocalDateTime.now();
-
-        String strDate = ldt.format(dtf);
-        System.out.println(strDate);
-
         DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("yyyyÄêMMÔÂddÈÕ HH:mm:ss");
         String strDate2 = dtf2.format(ldt);
         System.out.println(strDate2);
