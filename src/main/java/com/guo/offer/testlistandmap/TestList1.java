0f655f8e3994ea79d2ba1548998dbf09e86c44e6
guozifeng <602394902@qq.com>
2017-11-20 23:08:46 +0800

    test

---
+++ src/main/java/com/guo/offer/testlistandmap/TestList1.java
@@ -0,0 +1,18 @@
+package com.guo.offer.testlistandmap;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class TestList1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List list = new ArrayList();
+		list.add("2232");
+		list.add(232);
+		list.add(new Date());
+		System.out.println(list.get(0));
+	}
+
+}
