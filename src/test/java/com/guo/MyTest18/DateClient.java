8b07fff9f28326b604fb0f8c79cf4010d26e2106
guozifeng <602394902@qq.com>
2017-11-20 23:48:56 +0800

    test

--- src/test/java/com/guo/MyTest18/DateClient.java
+++ src/test/java/com/guo/MyTest18/DateClient.java
@@ -25,4 +25,30 @@ public class DateClient {
         System.out.println(strDate2);
 
 	}
+	
+	@Test
+	public void test2(){
+		//自定义格式化器
+        DateTimeFormatter dtf= DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        //获取当前日期和时间
+        LocalDateTime ldt=LocalDateTime.now();
+
+        //使用系统提供的格式来格式化
+        String strDate= ldt.format(DateTimeFormatter.ISO_DATE);
+        System.out.println("使用默认的格式："+strDate);//使用默认的格式：2017-11-04
+
+        //第一种自定义格式化方式
+        String strMyDateTime= ldt.format(dtf);
+        System.out.println("自定义格式化:"+strMyDateTime);//自定义格式化:2017-11-04 14:30:36
+
+        //第二种自定义格式化方式
+        String strMyDateTime2=dtf.format(ldt);
+        System.out.println("自定义格式化2:"+strMyDateTime2);//自定义格式化2:2017-11-04 14:30:36
+
+        //使用自定义的格式来反格式化字符串
+        String strDateMe="2017-11-02 15:30:22";
+        LocalDateTime ldtMe= ldt.parse(strDateMe,dtf);
+        System.out.println(ldtMe);//2017-11-02T15:30:22
+	}
 }
