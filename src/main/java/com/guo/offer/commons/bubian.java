acc29dc8f91b9c432991b9f6e21c3f17e8debed4
guozifeng <602394902@qq.com>
2017-12-03 23:10:45 +0800

    test

---
+++ src/main/java/com/guo/offer/commons/bubian.java
@@ -0,0 +1,46 @@
+package com.guo.offer.commons;
+
+import java.util.Vector;
+
+public class bubian {
+	
+	 private static Vector<Integer> vector = new Vector<Integer>();
+	 private static int count =0;
+	    public static void main(String[] args) {
+	        while (true) {
+	        	count++;
+	            for (int i = 0; i < 10; i++) {
+	                vector.add(i);
+	            }
+
+	            Thread removeThread = new Thread(new Runnable() {
+	                @Override
+	                public void run() {
+	                    for (int i = 0; i < vector.size(); i++) {
+	                        vector.remove(i);
+	                    }
+	                }
+	            });
+
+	            Thread printThread = new Thread(new Runnable() {
+	                @Override
+	                public void run() {
+	                    for (int i = 0; i < vector.size(); i++) {
+	                    	vector.get(i);
+	                        
+	                    }
+	                }
+	            });
+
+	            System.out.println((count));
+	            
+	            removeThread.start();
+	            printThread.start();
+
+	            //不要同时产生过多的线程，否则会导致操作系统假死
+	            while (Thread.activeCount() > 20);
+	        }
+	    }
+
+
+}
---
+++ src/main/java/com/guo/offer/commons/testWhile.java
@@ -0,0 +1,7 @@
+package com.guo.offer.commons;
+
+public class testWhile {
+	public static void main(String[] args) {
+		while(true);
+	}
+}
