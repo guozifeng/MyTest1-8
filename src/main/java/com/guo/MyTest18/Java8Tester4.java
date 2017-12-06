124ab5a68ee1c8c9538494d4650468f571482b69
guozifeng <602394902@qq.com>
2017-12-07 00:19:56 +0800

    test

--- src/main/java/com/guo/MyTest18/Java8Tester4.java
+++ src/main/java/com/guo/MyTest18/Java8Tester4.java
@@ -10,36 +10,37 @@ import org.junit.Test;
 
 public class Java8Tester4 {
 
-	@Test
-	public void testLocalDateTime() {
-		System.out.println("当前时间戳: " + Instant.now());
-		// 获取当前的日期时间
-		LocalDateTime currentTime = LocalDateTime.now();
-					
-		System.out.println("当前时间: " + currentTime);
+    @Test
+    public void testLocalDateTime() {
+        System.out.println("当前时间戳: " + Instant.now());
 
-		LocalDate date1 = currentTime.toLocalDate();
-		System.out.println("date1: " + date1);
+        // 获取当前的日期时间
+        LocalDateTime currentTime = LocalDateTime.now();
 
-		Month month = currentTime.getMonth();
-		int day = currentTime.getDayOfMonth();
-		int seconds = currentTime.getSecond();
+        System.out.println("当前时间: " + currentTime);
 
-		System.out.println("月: " + month + ", 日: " + day + ", 秒: " + seconds);
+        LocalDate date1 = currentTime.toLocalDate();
+        System.out.println("date1: " + date1);
 
-		LocalDateTime date2 = currentTime.withDayOfMonth(10).withYear(2012);
-		System.out.println("date2: " + date2);
+        Month month = currentTime.getMonth();
+        int day = currentTime.getDayOfMonth();
+        int seconds = currentTime.getSecond();
 
-		// 12 december 2014
-		LocalDate date3 = LocalDate.of(2014, Month.DECEMBER, 12);
-		System.out.println("date3: " + date3);
+        System.out.println("月: " + month + ", 日: " + day + ", 秒: " + seconds);
 
-		// 22 小时 15 分钟
-		LocalTime date4 = LocalTime.of(22, 15);
-		System.out.println("date4: " + date4);
+        LocalDateTime date2 = currentTime.withDayOfMonth(10).withYear(2012);
+        System.out.println("date2: " + date2);
 
-		// 解析字符串
-		LocalTime date5 = LocalTime.parse("20:15:30");
-		System.out.println("date5: " + date5);
-	}
+        // 12 december 2014
+        LocalDate date3 = LocalDate.of(2014, Month.DECEMBER, 12);
+        System.out.println("date3: " + date3);
+
+        // 22 小时 15 分钟
+        LocalTime date4 = LocalTime.of(22, 15);
+        System.out.println("date4: " + date4);
+
+        // 解析字符串
+        LocalTime date5 = LocalTime.parse("20:15:30");
+        System.out.println("date5: " + date5);
+    }
 }
---
+++ src/main/java/com/guo/MyTest18/a.java
@@ -0,0 +1,10 @@
+ package com.guo.MyTest18;
+
+ /**
+  * 
+  * @author Administrator
+  * @date 2017/12/07
+  */
+ public class a {
+
+}
