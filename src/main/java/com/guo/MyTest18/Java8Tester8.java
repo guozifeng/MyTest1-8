b724e3c9b8d75854dc05a3f5217697b62fa011c5
guozifeng <602394902@qq.com>
2017-12-07 22:58:36 +0800

    11

--- src/main/java/com/guo/MyTest18/Java8Tester8.java
+++ src/main/java/com/guo/MyTest18/Java8Tester8.java
@@ -2,14 +2,14 @@ package com.guo.MyTest18;
 
 public class Java8Tester8 {
 
-	final static String salutation = "Hello! ";
+    final static String salutation = "Hello! ";
 
-	public static void main(String args[]) {
-		GreetingService greetService1 = message -> System.out.println(salutation + message);
-		greetService1.sayMessage("Runoob");
-	}
+    public static void main(String args[]) {
+        GreetingService greetService1 = message -> System.out.println(salutation + message);
+        greetService1.sayMessage("Runoob");
+    }
 
-	interface GreetingService {
-		void sayMessage(String message);
-	}
+    interface GreetingService {
+        void sayMessage(String message);
+    }
 }
