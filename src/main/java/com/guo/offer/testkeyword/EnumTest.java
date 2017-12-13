1f8da3c732b421eef0437e88926205216dac64de
guozifeng <602394902@qq.com>
2017-12-13 19:16:45 +0800

    1

--- src/main/java/com/guo/offer/testkeyword/EnumTest.java
+++ src/main/java/com/guo/offer/testkeyword/EnumTest.java
@@ -48,7 +48,7 @@ public class EnumTest {
 	public enum Gender {
 		// 通过括号赋值,而且必须带有一个参构造器和一个属性跟方法，否则编译出错
 		// 赋值必须都赋值或都不赋值，不能一部分赋值一部分不赋值；如果不赋值则不能写构造器，赋值编译也出错
-		MAN("MAN"), WOMEN("WOMEN");
+		MAN("MANA"), WOMEN("WOMENA");
 
 		private final String value;
 
