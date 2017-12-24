345a21a95fdf5ba79d7f911a18f10bbe4af4cf61
guozifeng <602394902@qq.com>
2017-12-24 21:36:11 +0800

    11

---
+++ src/main/java/com/guo/interview/questions/TryCatchTest.java
@@ -0,0 +1,25 @@
+package com.guo.interview.questions;
+
+public class TryCatchTest {
+
+    public static void main(String[] args) {
+        System.out.println(test1());
+    }
+
+    private static Integer test1() {
+        try {
+            System.out.println("catch is main1");
+            System.out.println(1 / 0);
+            System.out.println("catch is main2");
+            return 3;
+        } catch (Exception e) {
+            System.out.println("catch is begin");
+            return 1;
+        } finally {
+            System.out.println("finally is begin");
+            return 2;
+        }
+        // return 4;
+    }
+
+}
