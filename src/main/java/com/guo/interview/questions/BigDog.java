ff4b9b80f1671827a089c32164970ae18c675f68
guozifeng <602394902@qq.com>
2017-12-24 21:24:10 +0800

    1

--- src/main/java/com/guo/interview/questions/BigDog.java
+++ src/main/java/com/guo/interview/questions/BigDog.java
@@ -1,5 +1,13 @@
 package com.guo.interview.questions;
 
+/**
+ * 
+ * 子抽象类继承父抽象类，必须显性调用父抽象类的显性构造器
+ * 
+ * @author Administrator
+ * @date 2017/12/24
+ * 
+ */
 abstract class Animal {
     private int age;
 
