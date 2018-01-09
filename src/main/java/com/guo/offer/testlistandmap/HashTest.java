b19edb2ed7225cea46c6c55b1a9d74f9c893b843
guozifeng <602394902@qq.com>
2018-01-09 09:18:03 +0800

    1

---
+++ src/main/java/com/guo/offer/testlistandmap/HashTest.java
@@ -0,0 +1,54 @@
+package com.guo.offer.testlistandmap;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Administrator
+ * @date 2018/01/09
+ */
+public class HashTest {
+    private int i;
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public int hashCode() {
+        return i % 10;
+    }
+
+    /*public boolean equals(Object object) {
+        if (object == null) {
+            return false;
+        }
+        if (object == this) {
+            return true;
+        }
+        if (!(object instanceof HashTest)) {
+            return false;
+        }
+        HashTest other = (HashTest)object;
+        if (other.getI() == this.getI()) {
+            return true;
+        }
+        return false;
+    }*/
+
+    public final static void main(String[] args) {
+        HashTest a = new HashTest();
+        HashTest b = new HashTest();
+        a.setI(1);
+        b.setI(1);
+        Set<HashTest> set = new HashSet<HashTest>();
+        set.add(a);
+        set.add(b);
+        System.out.println(a.hashCode() == b.hashCode());
+        System.out.println(a.equals(b));
+        System.out.println(set);
+    }
+}
