3192cfa420963313839fb6f730ba2bb6ac1e94e7
guozifeng <602394902@qq.com>
2018-01-22 18:29:00 +0800

    test

--- src/main/java/com/guo/interview/questions/RBTreeNode.java
+++ src/main/java/com/guo/interview/questions/RBTreeNode.java
@@ -1,29 +1,25 @@
 package com.guo.interview.questions;
 
-public class RBTreeNode
-{
-    //nil结点的data为这个常量
-    public static final int NULL_DATA = Integer.MIN_VALUE; 
+public class RBTreeNode {
+    // nil结点的data为这个常量
+    public static final int NULL_DATA = Integer.MIN_VALUE;
     public static final RBTreeNode NULL_NODE = new RBTreeNode(NULL_DATA, Color.BLACK, null, null, null);
-    
-    public enum Color
-    {
-        RED("red"),
-        BLACK("black");
-        
+
+    public enum Color {
+        RED("red"), BLACK("black");
+
         private String color;
-        private Color(String color)
-        {
+
+        private Color(String color) {
             this.color = color;
         }
-        
+
         @Override
-        public String toString()
-        {
+        public String toString() {
             return color;
         }
     }
-    
+
     private int data;
     private Color color;
     private RBTreeNode leftChild;
@@ -70,21 +66,19 @@ public class RBTreeNode
         this.parent = parent;
     }
 
-    public RBTreeNode(int data, Color color, RBTreeNode leftChild,
-            RBTreeNode rightChild, RBTreeNode parent) {
+    public RBTreeNode(int data, Color color, RBTreeNode leftChild, RBTreeNode rightChild, RBTreeNode parent) {
         this.data = data;
         this.color = color;
         this.leftChild = leftChild;
         this.rightChild = rightChild;
         this.parent = parent;
     }
-    
-    //创建一个结点，左右孩子均为nil结点
-    public RBTreeNode(int data)
-    {
+
+    // 创建一个结点，左右孩子均为nil结点
+    public RBTreeNode(int data) {
         RBTreeNode leftChild = createNullNode();
         RBTreeNode rightChild = createNullNode();
-        
+
         this.data = data;
         color = Color.RED;
         this.leftChild = leftChild;
@@ -93,53 +87,51 @@ public class RBTreeNode
         this.leftChild.parent = this;
         this.rightChild.parent = this;
     }
-    
-    //创建nil结点
-    public static RBTreeNode createNullNode()
-    {
+
+    // 创建nil结点
+    public static RBTreeNode createNullNode() {
         return new RBTreeNode(NULL_DATA, Color.BLACK, null, null, null);
     }
-    
-    public void leftRotate()
-    {
+
+    public void leftRotate() {
         RBTreeNode originParent = parent;
         RBTreeNode originRight = rightChild;
         rightChild = rightChild.leftChild;
         originRight.leftChild = this;
-        
-        if(this == originParent.getLeftChild())
+
+        if (this == originParent.getLeftChild())
             originParent.setLeftChild(originRight);
-        else    originParent.setRightChild(originRight);
+        else
+            originParent.setRightChild(originRight);
         originRight.setParent(originParent);
         parent = originRight;
-//      if(!rightChild.isNull())
-            rightChild.parent = this;
+        // if(!rightChild.isNull())
+        rightChild.parent = this;
     }
-    
-    public void rightRotate()
-    {
+
+    public void rightRotate() {
         RBTreeNode originParent = parent;
         RBTreeNode originLeft = leftChild;
         leftChild = leftChild.rightChild;
         originLeft.rightChild = this;
-        
-        if(this == originParent.getLeftChild())
+
+        if (this == originParent.getLeftChild())
             originParent.setLeftChild(originLeft);
-        else    originParent.setRightChild(originLeft);
+        else
+            originParent.setRightChild(originLeft);
         originLeft.setParent(originParent);
         parent = originLeft;
-//      if(!leftChild.isNull())
-            leftChild.parent = this;
+        // if(!leftChild.isNull())
+        leftChild.parent = this;
     }
-    
+
     @Override
     public String toString() {
         return data + " ---> " + color;
     }
-    
-    public boolean isNull()
-    {
+
+    public boolean isNull() {
         return data == NULL_DATA;
     }
-    
+
 }
