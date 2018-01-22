192bcfc0824d2e60e437be733bd360ee150f1d5e
guozifeng <602394902@qq.com>
2018-01-22 18:03:05 +0800

    test

--- src/main/java/com/guo/interview/questions/SortDemo2.java
+++ src/main/java/com/guo/interview/questions/SortDemo2.java
@@ -73,6 +73,23 @@ public final class SortDemo2 {
         System.out.println(Arrays.toString(array) + " shellSort");
     }
 
+    public void selectSort1(int[] a) {
+        int length = a.length;
+        for (int i = 0; i < length; i++) {// 循环次数
+            int key = a[i];
+            int position = i;
+            for (int j = i + 1; j < length; j++) {// 选出最小的值和位置
+                if (a[j] < key) {
+                    key = a[j];
+                    position = j;
+                }
+            }
+            a[position] = a[i];// 交换位置
+            a[i] = key;
+        }
+        System.out.println(Arrays.toString(a) + " selectSort1");
+    }
+
     public static void selectSort(int[] array) {
         int position = 0;
         for (int i = 0; i < array.length; i++) {
