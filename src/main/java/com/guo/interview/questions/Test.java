3c4454cd7dc063e24ca489ec67b5d3f087dd61b6
guozifeng <602394902@qq.com>
2018-01-22 18:34:12 +0800

    test

---
+++ src/main/java/com/guo/interview/questions/Test.java
@@ -0,0 +1,17 @@
+ package com.guo.interview.questions;
+
+ /**
+ * @author Administrator
+ * @date 2018/01/22
+ */
+public class Test {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println(Integer.MIN_VALUE);
+    }
+
+}
