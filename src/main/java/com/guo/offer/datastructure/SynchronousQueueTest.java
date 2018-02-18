177ba37743b965cddceb901304d954dcb6db5c4d
guozifeng <602394902@qq.com>
2018-02-18 13:18:10 +0800

    test

--- src/main/java/com/guo/offer/datastructure/SynchronousQueueTest.java
+++ src/main/java/com/guo/offer/datastructure/SynchronousQueueTest.java
@@ -4,71 +4,80 @@ import java.util.Random;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * 下面使用SynchronousQueue模拟： 最多只有一个产品的生产者-消费者模型
+ * 
+ * 消费者线程们 逐个请求消费产品 生产者线程们 逐个生产产品。
+ * 
+ * @author maguowei01
+ * 
+ */
 public class SynchronousQueueTest {
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
-        SynchronousQueue<String> queue = new SynchronousQueue();
-        // TODO Auto-generated method stub
-        for (int i = 0; i < 1; i++)
-            new Thread(new ThreadProducer(queue)).start();
-        for (int i = 0; i < 1; i++)
-            new Thread(new ThreadConsumer(queue)).start();
-    }
-}
-
-class ThreadProducer implements Runnable {
-    ThreadProducer(SynchronousQueue<String> queue) {
-        this.queue = queue;
+        // true保证生产或消费者线程以FIFO的顺序访问。
+        SynchronousQueue<Integer> queue = new SynchronousQueue<Integer>(true);
+        for (int i = 0; i < 1; ++i) {
+            new Customer(queue).start();
+        }
+        for (int i = 0; i < 1; ++i) {
+            new Product(queue).start();
+        }
     }
 
-    SynchronousQueue<String> queue;
-    static int cnt = 0;
+    static class Product extends Thread {
+        SynchronousQueue<Integer> queue;
 
-    public void run() {
-        String name = "";
-        int val = 0;
-        Random random = new Random(System.currentTimeMillis());
-        while (true) {
-            cnt = (cnt + 1) & 0xFFFFFFFF;
+        public Product(SynchronousQueue<Integer> queue) {
+            this.queue = queue;
+        }
 
-            try {
-                val = random.nextInt() % 15;
-                if (val < 5) {
-                    name = "offer name:" + cnt;
-                    queue.offer(name);
-                } else if (val < 10) {
-                    name = "put name:" + cnt;
-                    queue.put(name);
-                } else {
-                    name = "offer wait time and name:" + cnt;
-                    queue.offer(name, 1000, TimeUnit.MILLISECONDS);
+        @Override
+        public void run() {
+            while (true) {
+                int rand = new Random().nextInt(1000);
+                System.out.println("Thread Id:" + getId() + "  生产了一个产品：" + rand);
+                //System.out.println("Thread Id:" + getId() + " 等待两秒后运送出去...");
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                /*
+                 * offer()往queue里放一个element后立即返回，如果碰巧这个element被另一个thread取走了，
+                 * offer方法返回true，认为offer成功；否则返回false。
+                 * 也就是说offer不一定真正的插入的队列中，肯定没成功丢失了
+                 */
+
+                // queue.offer(rand); //注意offer与put方法的区别
+                try {
+                    /* 
+                     * put()往queue放进去一个element以后就一直wait直到有其他thread进来把这个element取走。
+                     */
+                    queue.put(rand);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
-}
 
-class ThreadConsumer implements Runnable {
-    ThreadConsumer(SynchronousQueue<String> queue) {
-        this.queue = queue;
-    }
+    static class Customer extends Thread {
+        SynchronousQueue<Integer> queue;
 
-    SynchronousQueue<String> queue;
+        public Customer(SynchronousQueue<Integer> queue) {
+            this.queue = queue;
+        }
 
-    public void run() {
-        String name;
-        while (true) {
-            try {
-                name = queue.take();
-                System.out.println("take " + name);
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    // 线程运行到queue.take()阻塞，直到Product生产一个产品queue.offer。
+                    System.out.println("Thread Id:" + getId() + " 消费了一个产品:" + queue.take());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                //System.out.println("-------------------Customer-----------------------");
             }
         }
     }
