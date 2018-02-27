e9280b7aab3068eeaaa3c0590b6323393fac7e55
guozifeng <602394902@qq.com>
2018-02-28 00:01:42 +0800

    test

---
+++ src/main/java/com/guo/offer/commons/testHashcode.java
@@ -0,0 +1,11 @@
+package com.guo.offer.commons;
+
+public class testHashcode {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Object o = new Object();
+        System.out.println(o.hashCode());
+    }
+
+}
