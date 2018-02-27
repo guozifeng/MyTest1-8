bf489e925533c783374e3eae9f90afc37fe236fd
guozifeng <602394902@qq.com>
2018-02-27 17:23:59 +0800

    test

--- src/main/java/com/guo/offer/commons/testWhile.java
+++ src/main/java/com/guo/offer/commons/testWhile.java
@@ -2,8 +2,19 @@ package com.guo.offer.commons;
 
 public class testWhile {
 	public static void main(String[] args) {
+	    int i = 0;
 		while(true) {
-            ;
+		    i++;
+		    System.out.println("================="+i);
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                 e.printStackTrace();
+            }
+            if(i == 50){
+                break;
+            }
         }
 	}
 }
---
+++ src/main/java/com/guo/offer/testlistandmap/T.java
@@ -0,0 +1,18 @@
+ package com.guo.offer.testlistandmap;
+
+ /**
+ * @author Administrator
+ * @date 2018/02/26
+ */
+public class T {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println(64>>2);
+        System.out.println(36<<1);
+    }
+
+}
