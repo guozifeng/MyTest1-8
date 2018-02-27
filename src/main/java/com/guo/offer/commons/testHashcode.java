a0aa697c56169724efe3531f8d2381f6ea1d1f6e
guozifeng <602394902@qq.com>
2018-02-28 00:05:47 +0800

    test

--- src/main/java/com/guo/offer/commons/testHashcode.java
+++ src/main/java/com/guo/offer/commons/testHashcode.java
@@ -5,7 +5,13 @@ public class testHashcode {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         Object o = new Object();
+        Object o1 = new Object();
         System.out.println(o.hashCode());
+        System.out.println(o1.hashCode());
+        System.out.println(o.equals(o1));
+        
+        System.out.println(o);
+        System.out.println(o1);
     }
 
 }
