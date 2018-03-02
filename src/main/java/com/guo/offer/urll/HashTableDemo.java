dba935b2f51b50038d1a3935e0a83c696b4a74f4
guozifeng <602394902@qq.com>
2018-03-02 23:19:48 +0800

    test

--- src/main/java/com/guo/offer/urll/HashTableDemo.java
+++ src/main/java/com/guo/offer/urll/HashTableDemo.java
@@ -6,8 +6,8 @@ public class HashTableDemo {
 
     public static void main(String args[]) {
         // Create a hash map
-        Hashtable balance = new Hashtable();
-        Enumeration names;
+        Hashtable<String, Double> balance = new Hashtable<String, Double>();
+        Enumeration<String> names;
         String str;
         double bal;
 
