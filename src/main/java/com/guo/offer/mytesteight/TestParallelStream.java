c0a1e7545c893f372273fdeb6973052e15a761c7
guozifeng <602394902@qq.com>
2018-03-16 16:03:29 +0800

    test

--- src/main/java/com/guo/offer/mytesteight/TestParallelStream.java
+++ src/main/java/com/guo/offer/mytesteight/TestParallelStream.java
@@ -2,7 +2,6 @@ package com.guo.offer.mytesteight;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Vector;
 import java.util.concurrent.TimeUnit;
