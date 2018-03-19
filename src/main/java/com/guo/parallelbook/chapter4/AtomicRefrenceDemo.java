ea9c59ea0a1eb281d8b71d8b66f597773c8b80e9
guozifeng <602394902@qq.com>
2018-03-19 14:14:29 +0800

    test

--- src/main/java/com/guo/parallelbook/chapter4/AtomicRefrenceDemo.java
+++ src/main/java/com/guo/parallelbook/chapter4/AtomicRefrenceDemo.java
@@ -12,9 +12,9 @@ public class AtomicRefrenceDemo {
         final AtomicReference<Integer> money = new AtomicReference<Integer>();
         money.set(19);
 
-        for (int i = 0; i < 100; i++) {
-            new Thread() {
-                public void run() {
+        new Thread() {
+            public void run() {
+                for (int i = 0; i < 100; i++) {
                     while (true) {
                         Integer m = money.get();
                         if (m < 20) {
@@ -23,21 +23,21 @@ public class AtomicRefrenceDemo {
                                 break;
                             }
                         } else {
-                            // System.out.println("余额大于20,无需充值");
+
+                            System.out.println("余额大于20,无需充值" + i);
                             break;
                         }
                     }
-
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }.start();
-
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
             }
+        }.start();
 
-        }
+        
 
         new Thread() {
             public void run() {
