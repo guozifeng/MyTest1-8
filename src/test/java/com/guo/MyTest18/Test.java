1c3c3cedd65e2e91570b1b59a58c7fb48111d085
guozifeng <602394902@qq.com>
2018-03-20 16:18:58 +0800

    test

---
+++ src/test/java/com/guo/MyTest18/Test.java
@@ -0,0 +1,20 @@
+ package com.guo.MyTest18;
+
+ /**
+ * @author Administrator
+ * @date 2018/03/20
+ */
+public class Test {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println((11+3*8));
+        System.out.println((11+3*8)/4);
+        System.out.println((11+3*8)/4%3);
+        System.out.println(8%3);
+    }
+
+}
