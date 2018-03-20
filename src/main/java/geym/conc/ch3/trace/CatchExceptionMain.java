c476d0ee08da2643cfda9257cc73782de37218ac
guozifeng <602394902@qq.com>
2018-03-20 15:31:36 +0800

    test

--- src/main/java/geym/conc/ch3/trace/CatchExceptionMain.java
+++ src/main/java/geym/conc/ch3/trace/CatchExceptionMain.java
@@ -7,20 +7,18 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
- * submit吃掉异常
- * 1. 用Future.get()获得异常
- * 2. try-catch
+ * submit吃掉异常 1. 用Future.get()获得异常 2. try-catch
+ * 
  * @author Geym
  *
  */
 public class CatchExceptionMain {
-	public static void main(String[] args) throws InterruptedException, ExecutionException {
-		ThreadPoolExecutor pools=new ThreadPoolExecutor(0, Integer.MAX_VALUE,
-                0L, TimeUnit.SECONDS,
-                new SynchronousQueue<Runnable>());
-		
-		for(int i=0;i<5;i++){
-			pools.submit(new DivTask(100,i));
-		}
-	}
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ThreadPoolExecutor pools
+            = new ThreadPoolExecutor(0, Integer.MAX_VALUE, 0L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>());
+
+        for (int i = 0; i < 5; i++) {
+            pools.submit(new DivTask(100, i));
+        }
+    }
 }
